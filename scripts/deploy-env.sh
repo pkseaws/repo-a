f83965d4bdd936d93a27bf2aa40591ce74689319
pkseaws <164235580+pkseaws@users.noreply.github.com>
2025-08-08 10:31:45 +0530

    Update deploy-env.sh

--- scripts/deploy-env.sh
+++ scripts/deploy-env.sh
@@ -5,20 +5,20 @@ fi
 
 if [[ $1 = "prod" ]]
 then
-    ENVN=$(awk '{ gsub(/[{},"]/,""); print $2 }' prod-config.json)
-    DB=$(awk '{ gsub(/[{},"]/,""); print $NF }' prod-config.json)
+    ENVN=$(awk '{ gsub(/[{},"]/,""); print $2 }' config/prod-config.json)
+    DB=$(awk '{ gsub(/[{},"]/,""); print $NF }' config/prod-config.json)
     echo
     echo "Deploying on $ENVN environment using Database $DB"
 elif [[ $1 = "dev" ]]
 then
-    ENVN=$(awk '{ gsub(/[{},"]/,""); print $2 }' dev-config.json)
-    DB=$(awk '{ gsub(/[{},"]/,""); print $NF }' dev-config.json)
+    ENVN=$(awk '{ gsub(/[{},"]/,""); print $2 }' config/dev-config.json)
+    DB=$(awk '{ gsub(/[{},"]/,""); print $NF }' config/dev-config.json)
     echo
     echo "Deploying on $ENVN environment using Database $DB"
 elif [[ $1 = "stage" ]]
 then
-    ENVN=$(awk '{ gsub(/[{},"]/,""); print $2 }' stage-config.json)
-    DB=$(awk '{ gsub(/[{},"]/,""); print $NF }' stage-config.json)
+    ENVN=$(awk '{ gsub(/[{},"]/,""); print $2 }' config/stage-config.json)
+    DB=$(awk '{ gsub(/[{},"]/,""); print $NF }' config/stage-config.json)
     echo
     echo "Deploying on $ENVN environment using Database $DB"
 else
