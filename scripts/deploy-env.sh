989c2eb079d34e5af3e9481bce469c28e3772ab4
pkseaws <164235580+pkseaws@users.noreply.github.com>
2025-08-08 10:47:07 +0530

    Update deploy-env.sh

--- scripts/deploy-env.sh
+++ scripts/deploy-env.sh
@@ -4,20 +4,17 @@ echo "Argument is missing with $0"
 exit 1
 fi
 
-if [[ $1 = "prod" ]]
-then
+if [ $1 = "prod" ]; then
     ENVN=$(awk '{ gsub(/[{},"]/,""); print $2 }' config/prod-config.json)
     DB=$(awk '{ gsub(/[{},"]/,""); print $NF }' config/prod-config.json)
     echo
     echo "Deploying on $ENVN environment using Database $DB"
-elif [[ $1 = "dev" ]]
-then
+elif [ $1 = "dev" ]; then
     ENVN=$(awk '{ gsub(/[{},"]/,""); print $2 }' config/dev-config.json)
     DB=$(awk '{ gsub(/[{},"]/,""); print $NF }' config/dev-config.json)
     echo
     echo "Deploying on $ENVN environment using Database $DB"
-elif [[ $1 = "stage" ]]
-then
+elif [ $1 = "stage" ]; then
     ENVN=$(awk '{ gsub(/[{},"]/,""); print $2 }' config/stage-config.json)
     DB=$(awk '{ gsub(/[{},"]/,""); print $NF }' config/stage-config.json)
     echo
