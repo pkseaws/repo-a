980111733b54169c59eb50d6289c35beb7f33c59
pkseaws <164235580+pkseaws@users.noreply.github.com>
2025-08-13 11:54:46 +0530

    Create multiaz.tf

---
+++ terraform/multiaz.tf
@@ -0,0 +1,16 @@
+ data "aws_availability_zones" "available" {
+      state = "available"
+    }
+
+ resource "aws_vpc" "vpc-new" {
+      cidr_block = "10.0.0.0/16"
+      
+    }
+
+resource "aws_subnet" "public" {
+      for_each          = data.aws_availability_zones.available.names
+      vpc_id            = aws_vpc.vpc-bew.id
+      availability_zone = each.value
+      cidr_block        = cidrsubnet(aws_vpc.vpc-new.cidr_block, 8, index(data.aws_availability_zones.available.names, each.value))
+      
+    }
