92acbe34a44034699cecc68ba739e1a554d5e86f
pkseaws <164235580+pkseaws@users.noreply.github.com>
2025-08-13 11:55:34 +0530

    Update multiaz.tf

--- terraform/multiaz.tf
+++ terraform/multiaz.tf
@@ -9,7 +9,7 @@
 
 resource "aws_subnet" "public" {
       for_each          = data.aws_availability_zones.available.names
-      vpc_id            = aws_vpc.vpc-bew.id
+      vpc_id            = aws_vpc.vpc-new.id
       availability_zone = each.value
       cidr_block        = cidrsubnet(aws_vpc.vpc-new.cidr_block, 8, index(data.aws_availability_zones.available.names, each.value))
       
