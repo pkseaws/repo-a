cc9b83c7e0f337bd141ad3dae73908f6ab8247b9
pkseaws <164235580+pkseaws@users.noreply.github.com>
2025-08-14 11:45:01 +0530

    Update multiaz.tf

--- terraform/multiaz.tf
+++ terraform/multiaz.tf
@@ -1,3 +1,16 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
 /* data "aws_availability_zones" "available" {
       state = "available"
     }
