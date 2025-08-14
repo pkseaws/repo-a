1e3054ddd91afced858022d4560f38587f4343f0
pkseaws <164235580+pkseaws@users.noreply.github.com>
2025-08-14 11:47:28 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -15,3 +15,11 @@ resource "aws_subnet" "cicd_sub" {
     Name = "cicd_sub1"
   }
 }
+
+    resource "aws_instance" "my_ec2_instance" {
+      ami           = data.aws_ami.amazon_ami.id
+      instance_type = "t2.micro"
+      tags = {
+        Name = "MyTerraformEC2"
+      }
+    }
